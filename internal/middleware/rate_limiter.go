e2b6436af7a591985d2d09fdb40dd3bb48c46069
chisssel <platov.artem.15@gmail.com>
2026-06-04 01:53:47 +0300

    feat: add middleware configure

---
+++ internal/middleware/rate_limiter.go
@@ -0,0 +1,36 @@
+package middleware
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
+)
+
+func RateLimiter(rdb *redis.Client) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ip := c.ClientIP()
+		key := "rate:" + ip
+
+		ctx := context.Background()
+		count, err := rdb.Incr(ctx, key).Result()
+		if err != nil {
+			c.Next()
+			return
+		}
+
+		if count == 1 {
+			rdb.Expire(ctx, key, time.Minute)
+		}
+
+		if count > 60 {
+			c.JSON(http.StatusTooManyRequests, gin.H{"error": "rate limit exceeded"})
+			c.Abort()
+			return
+		}
+
+		c.Next()
+	}
+}
